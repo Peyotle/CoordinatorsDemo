45fd4d870342668a4a645e77052785d3968790e3
Oleg Chernyshenko <oleg@tugboat.nz>
2019-07-02 18:44:20 +1200

    Initial Commit

---
+++ Coordinators/ViewController.swift
@@ -0,0 +1,15 @@
+// Copyright © 2019 Tugboat. All rights reserved.
+// Created on 2/07/19
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
